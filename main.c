7177f5f148d3b631537d4c48217732d665fbeec8
Adrien CARBONARO <adrien.carbonaro@edu.ece.fr>
2018-01-01 20:28:45 +0100

    Ajout paramètres requete

--- main.c
+++ main.c
@@ -8,12 +8,18 @@
 // Liens utiles : http://broux.developpez.com/articles/c/sockets/
 //                https://openclassrooms.com/courses/les-requetes-http         
 //--------------------------------------------------------------------
-// Rev 0.1 : 	- On demande le site à l'utilisateur
-//				- On affiche la requette
-//				- On affiche la réponse et le contenu
+// Rev 0.1 :   - On demande le site à l'utilisateur
+//             - On affiche la requette
+//             - On affiche la réponse et le contenu
+//--------------------------------------------------------------------
+// Rev 0.2 :   - Création d'une fonction pour créer la requette
+//               en fonction de l'url de l'utilisateur
+//             - Possibilité de mettre un code à la requete 
+//               pour faire des requetes avec paramètres
+//               ex : 192.168.1.17/?M
 //--------------------------------------------------------------------
 // Auteur      : Adrien CARBONARO
-// Mise à jour : 31/12/2017 20:00
+// Mise à jour : 01/01/2018 20:25
 //--------------------------------------------------------------------
 
 //--- Includes ---//
@@ -37,6 +43,47 @@ typedef int SOCKET;
 //--- Variables ---//
 char rep[1000];
 
+//--------------------------------------------------------------
+// Créer Requete
+//--------------------------------------------------------------
+// void creerRequete(char* buf, char* lien, char* code)
+//--------------------------------------------------------------
+// buf  : le buffer final qui contient la requete
+// lien : l'url saisie par l'utilisateur
+// code : le code en paramètre pour une requette de type
+//        192.168.1.17/?M
+//--------------------------------------------------------------
+// On crée la requete avec l'url de l'utilisateur
+// et le paramètre
+//--------------------------------------------------------------
+void creerRequete(char* buf, char* lien, char* code){
+
+   // On commence à écrire la requete
+   printf("--------\n");
+	printf("REQUETE :\n\n");
+	
+   // Le "/" signifie qu'on récupère la racine du serveur
+   // On peut écrire à la place /fichier.php
+   // pour récupérer un fichier en particulier
+   strcpy(buf, "GET /"); 
+
+	// Si il y a un code à un caractère //
+	if(strlen(code) == 1){
+		strcat(buf, "?");  // On ajoute le "?"
+		strcat(buf, code); // et le code (M, X, V ...)
+	}
+	
+	// On ajoute le reste de la requete dans le buffer //
+	strcat(buf, " HTTP/1.1\r\nHost: ");
+	strcat(buf, lien);
+	strcat(buf, "\r\n\r\n"); // Les requetes HTTP doivent se terminer par \r\n\r\n
+
+	// On affiche la requete
+	puts(buf);
+	printf("--------\n"); // Pour bien délimiter la requete à l'affichage
+
+}
+
 //--- Main ---//
 int main()
 {
@@ -45,37 +92,51 @@ int main()
    
    if(sock != -1)
    {
-      printf("Socket créée \n");
-      printf("Entrez le nom d'un site : ");
-      char lien[100];
-      scanf("%s", lien);
-      printf("Le nom de votre site est %s \n", lien);
-      const char* url = lien;
-      struct hostent* host = gethostbyname(url); // On récupère l'url
-      SOCKADDR_IN sin;
+   	char lien[100];
+   	char code[10];
+
+   	printf("Socket créée \n");
+
+      // On saisie l'url
+   	printf("Entrez le nom d'un site : ");
+   	scanf("%s", lien);
+   	fflush(stdin);
+   	printf("Le nom de votre site est %s \n", lien);
+
+      // On saisie le code de paramètre de requete
+   	printf("Entrez le code : ");
+   	scanf("%s", code);
+   	fflush(stdin);
+
+   	const char* url = lien;
+   	struct hostent* host = gethostbyname(url);
+   	SOCKADDR_IN sin;
       bcopy(host->h_addr, &(sin.sin_addr), host->h_length); // erreur inexpliquée mais programme marche
       sin.sin_family= AF_INET;
       sin.sin_port= htons(PORT);
-      //pas besoin de bind
+      
+      // On se connecte au serveur
       if(connect(sock, (SOCKADDR*)&sin, sizeof sin)!= -1)
       {
-         printf("connexion établie ! \n");
-         printf("--------\n");
-         printf("REQUETE :\n\n");
-         // On crée la requette
-         char* buf = "GET / HTTP/1.1\r\nHost: www.perdu.com\r\n\r\n";
-         puts(buf);
-         printf("--------\n");
+      	printf("connexion établie ! \n");
+      	char buf[1000];
+      	// On cre la requete
+         creerRequete(buf, lien, code);
+      	// On envoie la requete au serveur
          if(send(sock, buf, strlen(buf), 0)!=-1)
-         {
-            printf("Requete envoyée! \n");
+      	{
+         	printf("Requete envoyée! \n");
+      		// Tant qu'on reçoie des réponses du serveur
             while(recv(sock, rep, 1000,0)>0)
-            {
-               printf("je reçois !\n");
-               printf("%s", rep);
-            }
-            printf(" Fin requete \n");
+      		{
+      			printf("je reçois !\n");
+      			printf("%s", rep);
+      		}
+      		printf(" Fin requete \n");
+      	
          }else perror("requete non transmise  \n");
+      
       }else perror(" la connexion a echoué !\n");
+   
    }else perror("chkeltiha f la creation ! ");
 }
